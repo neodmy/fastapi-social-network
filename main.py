6b844daef913b2ee9f64eea9bc42ed057c97c073
David Yusta <david.yusta@guidesmiths.com>
2021-11-01 22:07:53 +0100

    "feat: add basic Post model and create post endpoint"

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
+from typing import Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/posts")
+def create_post(post: Post):
+    return {"data": post}
