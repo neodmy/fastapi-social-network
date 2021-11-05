1f047cdb154786075de6f2886de17804b161f447
David Yusta <david.yusta@guidesmiths.com>
2021-11-05 10:32:38 +0100

    "refactor: use pydantic schemas as response model"

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
-from typing import Optional
+from typing import List
 
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from sqlalchemy.orm import Session
 
 from .database import Base, engine, get_db
 from . import models
-from .schemas import PostBase, PostCreate
+from . import schemas
 
 app = FastAPI()
 
@@ -17,13 +17,13 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/posts")
+@app.get("/posts", response_model=List[schemas.Post])
 async def get_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
     return posts
 
 
-@app.get("/posts/{id}")
+@app.get("/posts/{id}", response_model=schemas.Post)
 def get_post(id: int, db: Session = Depends(get_db)):
     found_post = db.query(models.Post).filter(models.Post.id == id).first()
     if not found_post:
@@ -31,8 +31,8 @@ def get_post(id: int, db: Session = Depends(get_db)):
     return found_post
 
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_post(post: PostCreate, db: Session = Depends(get_db)):
+@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
     new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
@@ -40,8 +40,8 @@ def create_post(post: PostCreate, db: Session = Depends(get_db)):
     return new_post
 
 
-@app.put("/posts/{id}")
-def update_post(id: int, post: PostCreate, db: Session = Depends(get_db)):
+@app.put("/posts/{id}", response_model=schemas.Post)
+def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
     post_query = db.query(models.Post).filter(models.Post.id == id)
     if post_query.first() is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} was not found")
